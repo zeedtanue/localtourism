05d8537b5fbbe9621c9a9404d86cda20b85b430a
zeedtanue <zeedtanue@gmail.com>
2021-07-24 22:18:29 +0800

    added express server and test page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -13,12 +12,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Let's Go!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Encouraging Local Tourism
         </p>
 
         <div className={styles.grid}>
@@ -27,10 +25,6 @@ export default function Home() {
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
@@ -40,15 +34,15 @@ export default function Home() {
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          
+            <h2>Test  &rarr;</h2>
+            <a passHref href="/test">
+              <p>
+                Page Link
+              </p>
+          
+            </a>
+            
         </div>
       </main>
 
---
+++ pages/test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const test = () => {
+    return (
+        <div>
+            <p >
+                Encouraging Local Tourism
+            </p>
+        </div>
+    )
+}
+
+export default test
